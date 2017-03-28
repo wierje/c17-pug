1390a07ba17bcc778c3188d943bdae9eadd10032
wierje <wier.je@gmail.com>
2017-03-28 11:15:13 -0500

    fixes

--- app.js
+++ app.js
@@ -3,13 +3,17 @@
 const express = require('express');
 const app = express();
 
-app.use(static('public'));
+app.use(express.static('public'));
 
 app.set('view engine', 'pug');
 
 const names = ['Larry', 'Curly', 'Moe', 'Captain Kangaroo'];
 app.get('/', (req, res, next) => {
     res.render('index', { subtitle: "this came from my JS data", names, loggedIn: false })
+});
+
+app.get('/article', (req, res, next) => {
+    res.render('article', { subtitle: "this came from my JS data", names, loggedIn: false })
 });
 
 const port = process.env.PORT || 3000
